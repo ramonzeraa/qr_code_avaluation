1b7de77578fdf1ac48484b1dcf48bc6035db1f14
Ramon Cândido <ramoncandido64@gmail.com>
2026-04-15 03:00:13 +0200

    feat: creado generador del QRcode destino pantalla inicial

--- services/qr_service.py
+++ services/qr_service.py
@@ -4,8 +4,16 @@ import base64
 
 def generate_qr(url):
     qr = qrcode.make(url)
-
+    
+     # 👇 MOSTRAR NA TELA
+    qr.show()
+    print("QR code gerado com sucesso!")
     buffer = io.BytesIO()
     qr.save(buffer, format="PNG")
 
     return base64.b64encode(buffer.getvalue()).decode()
+
+if __name__ == "__main__":
+    url = "https://qr-code-avaluation.onrender.com/"
+    qr_base64 = generate_qr(url)
+    print("QR gerado com sucesso!")
